86415e22eb5d13519a36416b9c7b541903c6f12a
behzadk <behzad.karkaria@gmail.com>
2016-03-20 20:14:07 +0000

    Added pull of individual tablecards

--- UIReader/PokerUIReader.py
+++ UIReader/PokerUIReader.py
@@ -82,6 +82,7 @@ def takePokerWindowScreenshot(window):
     dcObj = win32ui.CreateDCFromHandle(wDC)
     cDC = dcObj.CreateCompatibleDC()
     dimensions = getClientDimensions(windowHandle)
+    print(dimensions)
     width = dimensions[2]
     height = dimensions[3]
 
@@ -102,16 +103,96 @@ def takePokerWindowScreenshot(window):
     win32gui.ReleaseDC(windowHandle, wDC)
     win32gui.DeleteObject(dataBitMap.GetHandle())
 
+
+def getTableCardsBMP(window):
+    '''
+    Eventual aim is to return table cards for each window?
+    or maybe return BMP and pass to another 
+    1. Make BMP of picture cards
+    2. Count number of cards
+    3. 
+    '''
+    windowHandle = window[0]
+    windowName = window[1]
+    
+    wDC = win32gui.GetWindowDC(windowHandle)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dimensions = getClientDimensions(windowHandle)
+    print(dimensions)
+    pwWidth = dimensions[2]
+    pwHeight = dimensions[3]
+    
+    allTableCardsBox = [int(0.33*pwWidth), int(0.388*pwHeight), int(0.38*pwWidth), int(0.085*pwHeight)] #All table cards
+    firstTableCardBox = [int(0.344*pwWidth), int(0.388*pwHeight), int(0.021*pwWidth), int(0.065*pwHeight)]
+    secondTableCardBox = [int(0.412*pwWidth), int(0.388*pwHeight), int(0.021*pwWidth), int(0.065*pwHeight)]
+    thirdTableCardBox = [int(0.480*pwWidth), int(0.388*pwHeight), int(0.021*pwWidth), int(0.065*pwHeight)]
+    fourthTableCardBox = [int(0.549*pwWidth), int(0.388*pwHeight), int(0.021*pwWidth), int(0.065*pwHeight)]
+    fithTableCardBox = [int(0.617*pwWidth), int(0.388*pwHeight), int(0.021*pwWidth), int(0.065*pwHeight)]
+
+    tableCardDimensionsArray = []
+    tableCardDimensionsArray.append(firstTableCardBox)
+    tableCardDimensionsArray.append(secondTableCardBox)
+    tableCardDimensionsArray.append(thirdTableCardBox)
+    tableCardDimensionsArray.append(fourthTableCardBox)
+    tableCardDimensionsArray.append(fithTableCardBox)
+    
+    tableCardsBMPArray = []
+    
+    for tableCard in tableCardDimensionsArray:
+        
+        print(tableCard)
+        width = tableCard[2]
+        height = tableCard[3]
+        
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, width, height)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (width, height), dcObj, (tableCard[0], tableCard[1]), win32con.SRCCOPY)
+        print(windowName)
+        
+        #Save file
+        timeStamp = '{:%Y-%m-%d %H-%M-%S-%f}'.format(datetime.datetime.now())
+        print(timeStamp)
+        dataBitMap.SaveBitmapFile(cDC, "C:\\temp\\" + timeStamp +".bmp")
+        
+        tableCardsBMPArray.append(dataBitMap)
+        
+        
+    #Freeing resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(windowHandle, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+        
+        
+    
+    return tableCardsBMPArray
+
+    
 if __name__ == '__main__':
     pokerWindows = findOpenPokerWindows()
     childWindows=[]
     coordinates = []
     
+    print(257/766)
+    print(201/518)
+    print(" ")
+    print(268/766)
+    print(40/518)
+    
+    print(260/766, 205/518, 14/766, 32/518)
+    print(314/766, 205/518, 14, 32)
+    print(368/766, 205/518, 14, 32)
+    print(422/766, 205/518, 14, 32)
+    print(476/766, 205/518, 14, 32)
+
+    
     childWindows = getChildWindows(pokerWindows)
     
     for window in pokerWindows:
         takePokerWindowScreenshot(window)
-    
+        tableCardsBMP = getTableCardsBMP(window)
  
     '''
     for window in childWindows:
